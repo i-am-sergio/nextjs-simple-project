c1e6ad25e6b4f67314c894adb364c65d2d2d8f47
sergio15dev <sergio15dev@gmail.com>
2023-09-11 17:55:16 -0500

    change styles with tailwind css

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -6,11 +6,15 @@ async function getUser(id) {
 
 async function UserPage({ params }) {
   const user = await getUser(params.id);
-  return <div className="bg-slate-400 p-10 rounded-md">
-    <img src={user.avatar} className="m-auto my-4"/>
-    <h3 className="text-3xl font-bold text-white">{user.id} {user.first_name} {user.last_name}</h3>
-    <p className="text-white">email: {user.email}</p>
-  </div>;
+  return (
+    <div className="bg-slate-400 p-10 rounded-md">
+      <img src={user.avatar} className="m-auto my-4" />
+      <h3 className="text-3xl font-bold text-white">
+        {user.id} {user.first_name} {user.last_name}
+      </h3>
+      <p className="text-white">email: {user.email}</p>
+    </div>
+  );
 }
 
 export default UserPage;
